ecef5f5c72a5c395367d8a3364bf6e73c90be522
Johnathan Bergman <jbergman@bsd220.org>
2021-01-25 10:44:40 -0600

    Inserted Trivial comments.

--- Git-R-Done/ViewController.swift
+++ Git-R-Done/ViewController.swift
@@ -8,12 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Insert Trivial Comment Here
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
